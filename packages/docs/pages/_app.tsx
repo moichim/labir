8a754e8f9d834bf63cf4e5993100064cfcdd4abf
Jan Jáchim <jachim5@gmail.com>
2024-06-24 18:04:09 +0200

    add webcomponents to every page

--- packages/docs/pages/_app.tsx
+++ packages/docs/pages/_app.tsx
@@ -1,6 +1,10 @@
+import Script from 'next/script'
 import '../assets/styles.css'
  
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Script src="https://cdn.jsdelivr.net/npm/@labir/embed/dist/embed.min.js" />
+  <Component {...pageProps} />
+  </>
 }
