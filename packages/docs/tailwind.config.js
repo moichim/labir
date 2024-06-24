9ddcbfe474d21a0540c366be56e5f21d199939c9
Jan Jáchim <jachim5@gmail.com>
2024-06-24 17:30:57 +0200

    added webcomponents

--- packages/docs/tailwind.config.js
+++ packages/docs/tailwind.config.js
@@ -1,22 +1,17 @@
-const { nextui } = require('@nextui-org/react');
-const path = require( "node:path" );
+const { nextui } = require("@nextui-org/react");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      './pages/**/*.{js,jsx,ts,tsx,md,mdx}',
-      './components/**/*.{js,jsx,ts,tsx,md,mdx}',
-      path.resolve( "../../node_modules/@labir/tailwind/**/*.{js,jsx,tsx,md,mdx}" ),
-      path.resolve( "../../node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}" ),
-   
-      // Or if using `src` directory:
-      './src/**/*.{js,jsx,ts,tsx,md,mdx}'
-    ],
-    theme: {
-      extend: {}
-    },
-    plugins: [
-        nextui()
-    ],
-    // important: "#__next",
-  }
+  content: [
+    "./pages/**/*.{js,jsx,ts,tsx,md,mdx}",
+    "./components/**/*.{js,jsx,ts,tsx,md,mdx}",
+    "../../node_modules/@labir/tailwind/**/*.{js,jsx,tsx,md,mdx}",
+    "../../node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    nextui()
+  ],
+};
