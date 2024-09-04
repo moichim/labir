01cfca4542174609a72777e4fbad6792635f75ca
Jan Jáchim <jachim5@gmail.com>
2024-09-04 10:31:06 +0200

    optimise embedded opacity slider

--- packages/embed/src/controls/registry/OpacitySlider.ts
+++ packages/embed/src/controls/registry/OpacitySlider.ts
@@ -1,36 +1,30 @@
-import { PropertyValueMap, css, html } from "lit";
-import { customElement, property } from "lit/decorators.js";
+import { consume } from "@lit/context";
+import { css, html } from "lit";
+import { customElement } from "lit/decorators.js";
 import { RegistryConsumer } from "../../hierarchy/consumers/RegistryConsumer";
+import { registryOpacityContext } from "../../hierarchy/providers/context/RegistryContext";
 
 @customElement("registry-opacity-slider")
 export class OpacityRangeElement extends RegistryConsumer {
 
-
-    @property({ type: Number, reflect: true, attribute: true })
+    @consume({context: registryOpacityContext, subscribe: true})
     value!: number;
 
     connectedCallback(): void {
         super.connectedCallback();
 
-        // this.log( this.registry );
-
-        // Set the default value from the registry
-        this.value = this.registry.opacity.value;
-
         // Handler of incoming changes
-        const handleChange = (value: number) => {
+        const handleIncomingChange = (value: number) => {
            
             // Proceed only when value is different
             if ( this.value !== value ) {
-                // Store the value
-                this.value = value;
                 // Update the DOM
                 this.renderRoot.querySelector<HTMLInputElement>( "#handler" )!.value = value.toString();
             }
         }
 
         // Register incoming changes
-        this.registry.opacity.addListener( this.UUID, handleChange.bind( this ) );
+        this.registry.opacity.addListener( this.UUID, handleIncomingChange.bind( this ) );
 
     }
 
@@ -40,24 +34,9 @@ export class OpacityRangeElement extends RegistryConsumer {
     }
 
     /** Handle user input events */
-    onInputChange( event: {target: {value: string}} ) {
+    handleUserChangeEvent( event: {target: {value: string}} ) {
         const value = parseFloat( event.target.value );
-        this.value = value;
         this.registry.opacity.imposeOpacity( value );
-    }
-
-    /** After update the DOM, impose the value to the registry */
-    protected updated(_changedProperties: PropertyValueMap<this> | Map<PropertyKey, unknown>): void {
-
-        super.updated( _changedProperties );
-
-        if ( _changedProperties.has( "value" ) ) {
-            const value = parseFloat( _changedProperties.get( "value" ) as string );
-            if ( value !== this.value ) {
-                this.registry.opacity.imposeOpacity( this.value );
-            }
-        }
-        
     }
 
 
@@ -79,7 +58,7 @@ export class OpacityRangeElement extends RegistryConsumer {
                 max="1"
                 step="0.01"
                 value="${this.value}"
-                @input="${this.onInputChange}"
+                @input="${this.handleUserChangeEvent}"
             />
             <slot></slot>
         `;
