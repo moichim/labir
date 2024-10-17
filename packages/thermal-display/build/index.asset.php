315d7596e66aa3e08b82e5697eef3a17171ef5ba
Jan Jáchim <jachim5@gmail.com>
2024-10-17 13:39:20 +0200

    Adjust package visibility

--- packages/thermal-display/build/index.asset.php
+++ packages/thermal-display/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '400aa83062ea5048d106');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2c1fde40fd3ed03aa79c');
--- packages/thermal-display/build/view.asset.php
+++ packages/thermal-display/build/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '024019c6859eda7893d4');
+<?php return array('dependencies' => array(), 'version' => '2198cbd194cc9f90546a');
