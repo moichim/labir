0504f3ce10591ed6864fd0f8b143e3dacdb04cde
Jan Jáchim <jachim5@gmail.com>
2024-11-08 17:18:57 +0100

    trying to implement webcomponents structure in @labir/wordpress

---
+++ packages/thermal-display/build/group/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '34c66f8b72369119e7c7');
---
+++ packages/thermal-display/build/manager/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '0e3ae4fb66f5c50cfe01');
